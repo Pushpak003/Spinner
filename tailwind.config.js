1090c85a8dea2a6a998fdc76365a2e5dd5294993
Pushpak003 <pushpakpathe63@gmail.com>
2025-08-01 03:28:11 +0530

    a Spinner UI for Someone i know

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        digital: ["'Press Start 2P'", "cursive"], // Winner number style ke liye
+        arcade: ["'Bungee'", "cursive"], // Heading style
+      },
+    },
+  },
+  plugins: [],
+};
